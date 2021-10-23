ea701ca52a18b36c809e3c5ea9122d65e8ef88d4
Tuấn Hwing <tuanhwing2014@gmail.com>
2021-10-23 14:59:35 +0700

    fix (rebase) : resolve conflict

--- th_secure_storage/test/th_secure_storage_test.dart
+++ th_secure_storage/test/th_secure_storage_test.dart
@@ -1,12 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:th_secure_storage/th_secure_storage.dart';
+
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  late SharedPreferences preferences;
 
   setUp(() async {
+    preferences = await SharedPreferences.getInstance();
   });
 
   test('shared preferences', () async {
-
+    int counter = 1;
+    // preferences.setInt('counter', counter);
+    // preferences.getInt('counter');
+    // expect(preferences.getInt('counter'), counter);
+    //
+    // preferences.remove('counter');
+    // expect(prefs.getInt('counter'), null);
   });
 }
