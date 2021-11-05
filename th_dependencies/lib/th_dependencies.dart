ddf151ee69def49caf8d458ced5b7c42f147b4cd
Tuấn Hwing <tuanhwing2014@gmail.com>
2021-11-05 14:36:33 +0700

    feat (common) : Apply logic show loading + error in base state

--- th_dependencies/lib/th_dependencies.dart
+++ th_dependencies/lib/th_dependencies.dart
@@ -3,3 +3,4 @@ library th_dependencies;
 export 'package:flutter_secure_storage/flutter_secure_storage.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:get_it/get_it.dart';
+export 'package:equatable/equatable.dart';
