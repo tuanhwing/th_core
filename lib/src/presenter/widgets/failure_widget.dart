c40b738cb43e333951c0b831bf927660f3ecf577
Tuấn Hwing <tuanhwing2014@gmail.com>
2023-12-01 10:48:37 +0700

    fix (failure_widget) : relayout subviews in failure widget

--- lib/src/presenter/widgets/failure_widget.dart
+++ lib/src/presenter/widgets/failure_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:th_core/src/extensions/extensions.dart';
 import 'package:th_core/src/resources/th_dimens.dart';
@@ -7,61 +6,94 @@ import 'package:th_dependencies/th_dependencies.dart';
 ///Failure widget
 class FailureWidget extends StatelessWidget {
   ///Constructor
-  const FailureWidget(
-      {super.key, this.errorMessage, this.titleButton, this.onRetry,});
+  const FailureWidget({
+    super.key,
+    this.title,
+    this.description,
+    this.titleButton,
+    this.onRetry,
+    this.padding,
+  });
 
-  ///Error message
-  final String? errorMessage;
+  ///Title
+  final String? title;
+
+  ///Message
+  final String? description;
+
   ///Title button
   final String? titleButton;
+
   ///Retry callback
   final VoidCallback? onRetry;
+
+  ///Padding
+  final EdgeInsetsGeometry? padding;
 
   @override
   Widget build(BuildContext context) {
     final ThemeData themeData = Theme.of(context);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Image.asset('assets/images/error.png', package: 'th_core',),
-        Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(THDimens.size24),
-          child: Text(
-            '${errorMessage ?? tr('something_went_wrong').inCaps} :(',
-            style: themeData.textTheme.titleLarge!
-                .apply(color: themeData.primaryColor),
-            textAlign: TextAlign.center,
-          ),
-        ),
+    final String descriptionStr = description ?? '';
 
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(
-              Radius.circular(THDimens.size16),
-            ),
-            border: Border.all(
-              color: Theme.of(context).primaryColor,
+    return Padding(
+      padding: padding ?? const EdgeInsets.all(THDimens.size16),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: THDimens.size32),
+            child: Image.asset(
+              'assets/images/error.png',
+              package: 'th_core',
             ),
           ),
-          child: InkWell(
-            borderRadius: BorderRadius.circular(THDimens.size16),
-            onTap: onRetry,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: THDimens.size8,
-                horizontal: THDimens.size32,
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(THDimens.size8),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  '${title ?? tr('something_went_wrong').allInCaps}!',
+                  style: themeData.textTheme.titleLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                if (descriptionStr.isNotEmpty)
+                  Text(
+                    descriptionStr.inCaps,
+                    style: themeData.textTheme.bodyLarge
+                        ?.copyWith(color: themeData.hintColor),
+                  )
+                else
+                  const SizedBox(),
+              ],
+            ),
+          ),
+          if (onRetry != null)
+            TextButton(
+              onPressed: onRetry,
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                  const EdgeInsets.symmetric(
+                    horizontal: THDimens.size16,
+                  ),
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(THDimens.size8),
+                    side: BorderSide(color: themeData.disabledColor),
+                  ),
+                ),
               ),
               child: Text(
                 titleButton ?? tr('retry').inCaps,
-                style: Theme.of(context).textTheme.titleMedium!.apply(
-                  fontWeightDelta: 2,
-                ),
+                style: const TextStyle(fontSize: THDimens.size16),
               ),
-            ),
-          ),
-        ),
-      ],
+            )
+          else
+            const SizedBox(),
+        ],
+      ),
     );
   }
 }
